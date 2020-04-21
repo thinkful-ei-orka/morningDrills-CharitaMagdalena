6d1261957f6b41538d3eb2fde749fafabc9b7749
Charita Nichols <nichols.charita@gmail.com>
2020-04-21 18:45:08 -0400

    more updates

--- array1.js
+++ array1.js
@@ -1,11 +1,12 @@
+// eslint-disable-next-line no-unused-vars
 function average(numbers) {
     
 
     let total = 0;
-    
+    numbers = [1,5,7,9] 
   
   numbers.forEach(function(item) {
       total += item;
-  }
+  })
   return total/ numbers.length;
 }
